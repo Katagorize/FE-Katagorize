d284ce5cfe24adf243e049900182f764918c0fe7
MtlJ1991 <MtlJ1991@gmail.com>
2018-01-25 16:14:42 +0000

    nav bar added

--- src/components/KataPage.js
+++ src/components/KataPage.js
@@ -21,7 +21,15 @@ class KataPage extends React.Component {
         return (
             
         <div className='KataPage'>
-    
+
+            <div class="topnav" id="myTopnav">
+            <a href="#home" class="active"><i class="fa fa-home" aria-hidden="true"></i></a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+            </div>
+                
              <div className='title'>
                 <h1>KataLyst</h1>
              </div>
