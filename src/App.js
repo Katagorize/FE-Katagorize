6f096627b303fefb157149605ac824d6a04963cd
MtlJ1991 <MtlJ1991@gmail.com>
2018-01-25 09:51:40 +0000

    added SRC react app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import Particles from 'react-particles-js'
+import pParams from './particles/particles'
+import HomePage from './HomePage';
+class App extends Component {
+
+
+  render() {
+
+    return (
+      <div className="App">
+
+          <HomePage />
+
+
+        </div>
+
+
+     
+    );
+  }
+}
+
+export default App;
