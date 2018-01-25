207fc45f064b3d4b18e0d5410d590c279e343bd2
MtlJ1991 <MtlJ1991@gmail.com>
2018-01-25 10:27:09 +0000

    added dataVisualisation, react circle-progress

--- src/components/KataPage.js
+++ src/components/KataPage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 import pParams from '../particles/particles';
+import '/home/mtlj1991/Documents/Northcoders/project-phase/front-end/FE-Katagorize/src/css/style.css'
+import CircularProgressbar from 'react-circular-progressbar';
+
 
 
 class KataPage extends React.Component {
 
     state = {
-        username: '',
-        password: ''
-    }
+        num: 0
+      }
+      
+      numChange = () => {
+        this.setState ({
+          num: (this.state.num+10) % 101
+        })
+        console.log(this.state.num)
+       }
 
     render() {
 
@@ -31,6 +40,29 @@ class KataPage extends React.Component {
 
              <div className='results'>
              <p>testetstetststetstes</p>
+             <CircularProgressbar percentage={60} 
+          
+                strokeWidth={5}
+                Clockwise
+                initialAnimation={true}
+                strokeWidth={8}
+             />
+
+
+            <CircularProgressbar percentage={this.state.num} 
+          
+                strokeWidth={5}
+                Clockwise
+                initialAnimation={true}
+                strokeWidth={8}
+                textForPercentage={(percentage) => {
+                    return percentage === 100 ? `Woo!!` : `${this.state.num}`;
+                }}
+                classForPercentage={(percentage) => {
+                    return percentage === 100 ? 'complete' : 'incomplete';
+                }}
+            />
+          <button type="button" onClick={this.numChange}>Click Me!</button>
              </div>
 
         
