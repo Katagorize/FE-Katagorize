b6e13df2386e384ce52748c510bbc13323f4b1a2
MtlJ1991 <MtlJ1991@gmail.com>
2018-01-29 18:19:55 +0000

    katas rendering based on user

--- src/components/KataPage.js
+++ src/components/KataPage.js
@@ -13,31 +13,49 @@ class KataPage extends React.Component {
         katas: []
     }
 
+    pleaseWork () {
+        return fetch(`http://localhost:3001/api/users/${this.props.match.params.username}`)
+        .then((katas) => {
+           return katas.json()
+        })
+        .then((katas) => {
+            this.setState({
+                katas: katas
+            })
+        })
+    }
 
+componentDidMount() {
+     this.pleaseWork();
+}
 
     render() {
-        console.log(data)
-
+        console.log(this.state.katas)
         return (
             <div>
 
                 <div className='KataPage'>
 
-                    <div class="topnav" id="myTopnav">
-                        <a href="/"><i class="fa fa-home" aria-hidden="true"></i></a>
-                        <a href="/kata" class="active">Katas</a>
+                    {/* <div className="topnav" id="myTopnav">
+                        <a href="/"><i className="fa fa-home" aria-hidden="true"></i></a>
+                        <a href="/kata" className="active">Katas</a>
                         <a href="#contact">Contact</a>
                         <a href="#about">About</a>
-                        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-                    </div>
+                        <a href="javascript:void(0);" className="icon" onClick="myFunction()">&#9776;</a>
+                    </div> */}
 
                     <div className='title'>
                         <h2>KataLyst testing area</h2>
+
                     </div>
 
 
                     <div className='KataList'>
-                        
+                    {this.state.katas.map((kata) => {
+                       return (
+                            <li>{kata.kata_name}</li>
+                       )
+                        })}
                     </div>
                     <KataData />
                    
