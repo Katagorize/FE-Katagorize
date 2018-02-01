a304d215fd616f5903893161d9aad549076fb05c
James Steele <jamessteele87@outlook.com>
2018-02-01 12:54:31 +0000

    add kataname to test data

--- src/components/KataData.js
+++ src/components/KataData.js
@@ -3,7 +3,7 @@ import '../css/style.css';
 import '../css/KataData.css';
 import CircularProgressbar from 'react-circular-progressbar';
 // import Trend from 'react-trend';
-import {Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 
 class KataData extends React.Component {
@@ -43,20 +43,20 @@ class KataData extends React.Component {
             })
     }
 
-   componentDidMount() {
+    componentDidMount() {
         this.getUserScores()
-        .then(()=> {
-            this.getAllData()
-        })
+            .then(() => {
+                this.getAllData()
+            })
     }
- 
+
 
     componentWillReceiveProps(newProps) {
         this.getUserScores(newProps)
-        .then(() => {
-            this.getAllData();
-        })
-    
+            .then(() => {
+                this.getAllData();
+            })
+
     }
 
     getUserScores = (props) => {
@@ -68,59 +68,58 @@ class KataData extends React.Component {
             username = props.match.params.username
             kataname = props.match.params.kata_name
         }
-  
+
         return fetch(`http://katalystpro-env.eu-west-2.elasticbeanstalk.com/api/users`)
             .then((res) => res.json())
             .then((userKatas) => {
-              return userKatas[username][kataname].map((score) => {
-                  return Number(score.match(/(\d+)(?!.*\d)/)[0])
-              })
+                return userKatas[username][kataname].map((score) => {
+                    return Number(score.match(/(\d+)(?!.*\d)/)[0])
+                })
             })
             .then((scores) => {
-            return this.setState({scores})
+                return this.setState({ scores })
             })
     }
-     removeDuplicates = (arr) => {
+    removeDuplicates = (arr) => {
         const filteredArr = []
-        for(let i = 0; i < arr.length; i++){
-          if(i === 0) filteredArr.push(arr[i])
-          else if(arr[i] !== arr[i - 1]) filteredArr.push(arr[i])
+        for (let i = 0; i < arr.length; i++) {
+            if (i === 0) filteredArr.push(arr[i])
+            else if (arr[i] !== arr[i - 1]) filteredArr.push(arr[i])
         }
         return filteredArr
-      }
+    }
 
     render() {
-        
+        let kataname = this.props.match.params.kata_name;
         const data = {
             labels: this.removeDuplicates(this.state.scores),
             datasets: [
                 {
-                  label: 'Test score',
-                  fill: true,
-                  lineTension: 0.1,
-                //   backgroundColor: '#083D77',
-                  borderColor: '#C31433',
-                  borderCapStyle: 'butt',
-                  borderDash: [],
-                  borderDashOffset: 0.0,
-                  borderJoinStyle: 'round',
-                  pointBorderColor: '#C31433',
-                  pointBackgroundColor: '#395E66',
-                  pointBorderWidth: 3,
-                  pointHoverRadius: 5,
-                  pointHoverBackgroundColor: '#C31433',
-                  pointHoverBorderColor: '#C31433',
-                  pointHoverBorderWidth: 3,
-                  pointRadius: 1,
-                  pointHitRadius: 10,
-                  data: this.removeDuplicates(this.state.scores)
+                    label: 'Test score',
+                    fill: true,
+                    lineTension: 0.1,
+                    borderColor: '#C31433',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'round',
+                    pointBorderColor: '#C31433',
+                    pointBackgroundColor: '#395E66',
+                    pointBorderWidth: 3,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: '#C31433',
+                    pointHoverBorderColor: '#C31433',
+                    pointHoverBorderWidth: 3,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: this.removeDuplicates(this.state.scores)
                 }
-              ]
+            ]
         }
         return (
             <div className="results">
                 <div className='resultsTitle'>
-                    <h4>Kata data</h4>
+                    <h4 className='testTitle'>{kataname} test data</h4>
                 </div>
 
                 <div className='circleDivA'>
@@ -151,25 +150,14 @@ class KataData extends React.Component {
                 </div>
 
                 <div className='graph'>
-
-                    {/* <h4>Scores for each test.</h4> */}
-                    {/* <Trend data={this.state.scores}
-                        autoDraw
-                        autoDrawDuration={4000}
-                        autoDrawEasing="ease-out" 
-                        gradient={['#C31433', '#395E66', '#083D77', '#DDE2C6']}
-                        width={300} 
-                        height={250}
-                        strokeWidth={4}/> */}
-
-                        <Line data={data}
+                    <Line data={data}
                         height={300}
                         width={300} />
-                    <p>This graph shows the highs and lows of your scores, you should be aiming high. The straighter the line, the more consistent you are with your code. </p>    
+                    <p>This graph shows the highs and lows of your scores, you should be aiming high. The straighter the line, the more consistent you are with your code. </p>
                 </div>
 
                 <div className='failBox'>
-                <h6>Here are the tests that you have failed.</h6>
+                    <h6>Here are the tests that you have failed.</h6>
                     {this.state.failureMessage.map((fails) => {
                         return (
                             <div className='BoxContent'><i className="fa fa-times-circle fa-lg failed" aria-hidden="true"></i><p>{fails.title}</p></div>
@@ -178,7 +166,7 @@ class KataData extends React.Component {
                 </div>
 
                 <div className='passBox'>
-                <h6>Here are the tests that you have passed.</h6>
+                    <h6>Here are the tests that you have passed.</h6>
 
                     {this.state.passMessages.map((passes) => {
                         return (
@@ -186,9 +174,6 @@ class KataData extends React.Component {
                         )
                     })}
                 </div>
-
-                
-
             </div>
         )
     }
