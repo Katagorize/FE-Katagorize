c24235eb9ac34e6994acbaf9ee02f5437fbda05b
James <bur1882@hotmail.co.uk>
2018-02-01 11:16:05 +0000

    Cretate kata data style (#10)
    * adjust layout of kata results to include a graph
    
    * minor adjustment to passes styling to the right
    
    * add hardcoded graph to track test scores
    
    * add functionality to graph with user scores
    
    * detailed text added, fixed circles rendering
    
    * circles enlarged, css changes
    
    * background color changed, color scheme added
    
    * adjust layout of kata results to include a graph
    
    * minor adjustment to passes styling to the right
    
    * add hardcoded graph to track test scores
    
    * add functionality to graph with user scores
    
    * detailed text added, fixed circles rendering
    
    * circles enlarged, css changes
    
    * background color changed, color scheme added
    
    * rebased and styled accordingly
    
    * adjust layout of kata results to include a graph
    
    * minor adjustment to passes styling to the right
    
    * add hardcoded graph to track test scores
    
    * add functionality to graph with user scores
    
    * detailed text added, fixed circles rendering
    
    * circles enlarged, css changes
    
    * background color changed, color scheme added
    
    * resolve merge conflict and typos
    
    * resolve merge conflict and typos
    
    * minor style changes

--- src/components/KataData.js
+++ src/components/KataData.js
@@ -2,74 +2,94 @@ import React from 'react';
 import '../css/style.css';
 import '../css/KataData.css';
 import CircularProgressbar from 'react-circular-progressbar';
+import Trend from 'react-trend';
 
 
 class KataData extends React.Component {
-    
+
     state = {
         tests: 0,
         passes: 0,
         fails: 0,
         passMessages: [],
-        failureMessage: []
+        failureMessage: [],
+        scores: [0, 0]
     }
-    
+
     getAllData = (newProps) => {
 
         let username = this.props.match.params.username;
         let kataname = this.props.match.params.kata_name;
 
         if (newProps) {
-            
+
             username = newProps.match.params.username
             kataname = newProps.match.params.kata_name
         }
-        
+
         return fetch(`http://katalystpro-env.eu-west-2.elasticbeanstalk.com/api/users/${username}/katas/${kataname}/test`)
-        .then((data) => {
-            return data.json()
-        })
-        .then((data) => {
-           return this.setState({
-                tests: data.stats.tests,
-                passes: data.stats.passes,
-                fails: data.stats.failures,
-                passMessages: data.passes,
-                failureMessage: data.failures
+            .then((data) => {
+                return data.json()
             })
+            .then((data) => {
+                return this.setState({
+                    tests: data.stats.tests,
+                    passes: data.stats.passes,
+                    fails: data.stats.failures,
+                    passMessages: data.passes,
+                    failureMessage: data.failures
+                })
+            })
+    }
+
+   componentDidMount() {
+        this.getUserScores()
+        .then(()=> {
+            this.getAllData()
         })
     }
-    
-    
-    componentDidMount() {
-        this.getAllData()
-    } 
-    
+ 
+
     componentWillReceiveProps(newProps) {
-        this.getAllData(newProps)
-    }
+        this.getUserScores(newProps)
+        .then(() => {
+            this.getAllData();
+        })
     
+    }
+
+    getUserScores = (props) => {
+        let username = this.props.match.params.username;
+        let kataname = this.props.match.params.kata_name;
+
+        if (props) {
+
+            username = props.match.params.username
+            kataname = props.match.params.kata_name
+        }
+  
+        return fetch(`http://katalystpro-env.eu-west-2.elasticbeanstalk.com/api/users`)
+            .then((res) => res.json())
+            .then((userKatas) => {
+              return userKatas[username][kataname].map((score) => {
+                  return Number(score.match(/(\d+)(?!.*\d)/)[0])
+              })
+            })
+            .then((scores) => {
+                console.log(scores)
+            return this.setState({scores})
+            })
+    }
+
     render() {
         return (
             <div className="results">
-                <h4>Kata data</h4>
-
-                <div className='circleDiv'>
-                    <CircularProgressbar  
-                        percentage={10}
-                        className="CircularProgressbar-inverted"
-                        strokeWidth={5}
-                        Clockwise
-                        initialAnimation={true}
-                        textForPercentage={(percentage) => {
-                            return percentage === 100 ? `Woo!!` : `${this.state.fails} / ${this.state.tests}`;
-                        }}
-                    />
-                    <p>Tests failed</p>
+                <div className='resultsTitle'>
+                    <h4>Kata data</h4>
                 </div>
 
-                <div className='circleDiv'>
-                    <CircularProgressbar  
+                <div className='circleDivA'>
+                    <CircularProgressbar
                         percentage={100 / this.state.tests * this.state.passes}
                         strokeWidth={5}
                         Clockwise
@@ -85,7 +105,7 @@ class KataData extends React.Component {
                 </div>
 
 
-                <div className='circleDiv'>
+                <div className='circleDivB'>
                     <CircularProgressbar
                         percentage={Math.ceil(100 / this.state.tests * this.state.passes)}
                         strokeWidth={5}
@@ -95,21 +115,41 @@ class KataData extends React.Component {
                     <p>Percentage complete</p>
                 </div>
 
+                <div className='graph'>
+
+                    <h4>Scores for each test.</h4>
+                    <Trend data={this.state.scores}
+                        autoDraw
+                        autoDrawDuration={4000}
+                        autoDrawEasing="ease-out" 
+                        gradient={['#C31433', '#395E66', '#083D77', '#DDE2C6']}
+                        width={300} 
+                        height={250}
+                        strokeWidth={4}/>
+                    <p>This graph shows the highs and lows of your scores, you should be aiming high. The straighter the line, the more consistent you are with your code. </p>    
+                </div>
+
                 <div className='failBox'>
+                <h6>Here are the tests that you have failed.</h6>
                     {this.state.failureMessage.map((fails) => {
                         return (
-                        <span><i className="fa fa-times-circle fa-lg" aria-hidden="true"></i><p>{fails.title}</p></span>
+                            <div className='BoxContent'><i className="fa fa-times-circle fa-lg failed" aria-hidden="true"></i><p>{fails.title}</p></div>
                         )
                     })}
                 </div>
 
                 <div className='passBox'>
+                <h6>Here are the tests that you have passed.</h6>
+
                     {this.state.passMessages.map((passes) => {
                         return (
-                        <span><i className="fa fa-check fa-lg" aria-hidden="true"></i><p>{passes.title}</p></span>
+                            <div className='BoxContent'><i className="fa fa-check fa-lg passed" aria-hidden="true"></i><p>{passes.title}</p></div>
                         )
                     })}
                 </div>
+
+                
+
             </div>
         )
     }
