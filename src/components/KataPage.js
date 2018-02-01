3a2b6fb3871539f8496cf04da64a25eb8fce756d
Semra Dora <semra.refkaewa@gmail.com>
2018-02-01 15:44:52 +0000

    add progress bar for each kata on the kata list

--- src/components/KataPage.js
+++ src/components/KataPage.js
@@ -14,14 +14,15 @@ import {
 import '../css/KataList.css';
 
 class KataPage extends React.Component {
-    
+
 
     state = {
         katas: [],
         allKatas: allKatas,
         userName: this.props.match.params.username,
-        kataName: this.props.match.kata_name,
+        kataName: this.props.match,
         hasClickedOnKata: false,
+        progress: {}
     }
 
     fetchKataList() {
@@ -40,13 +41,28 @@ class KataPage extends React.Component {
             })
     }
 
+    fetchProgress = () => {
+        return fetch(`http://katalystpro-env.eu-west-2.elasticbeanstalk.com/api/users`)
+            .then((res) => res.json())
+            .then((userKatas) => {
+                let kataScores = {}
+                for (let key in userKatas[this.props.match.params.username]) {
+                    let score = userKatas[this.props.match.params.username][key].reverse()[0].split(' ').reverse()[0]
+                    kataScores[key] = +score
+                }
+                this.setState({
+                    progress: kataScores
+                })
+            })
+    }
 
     componentDidMount() {
         this.fetchKataList();
+        this.fetchProgress()
     }
 
     render() {
-        console.log(this.state.katas)
+        console.log(this.state.progress)
         return (
             <div>
                 <div className='KataPage'>
@@ -71,9 +87,16 @@ class KataPage extends React.Component {
                                                                         <p>{kata.title}</p>
                                                                     </Link>
                                                                 </div>
-                                                                <div className="">
-                                                                    Put something here
+                                                                <div className='progress-div'>
+
+                                                                    <div className="progress-left" style={{ width: `${this.state.progress[kata.title]}%` }}>
+                                                                    </div>
+
+                                                                    <div className="progress-right" style={{width: `${100 - this.state.progress[kata.title]}%`}}>
+                                                                    </div >
+
                                                                 </div>
+
                                                             </div>
 
                                                         )
